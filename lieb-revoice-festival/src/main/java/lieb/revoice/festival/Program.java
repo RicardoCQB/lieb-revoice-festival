18bd4a713cb2159f54886020f189ca05383a9d58
RicardoCQB <ricardo_cqb@hotmail.com>
2020-04-14 15:27:50 +0100

    It works now!!

---
+++ lieb-revoice-festival/src/main/java/lieb/revoice/festival/Program.java
@@ -0,0 +1,13 @@
+package lieb.revoice.festival;
+
+import de.gurkenlabs.litiengine.Game;
+
+public class Program {
+
+	public static void main(String[] args) {
+		Game.init(args);
+		Game.start();
+
+	}
+
+}
