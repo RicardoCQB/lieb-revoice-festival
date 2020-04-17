253b1accc438dbc401615408fe20a59f74f64e78
RicardoCQB <ricardo_cqb@hotmail.com>
2020-04-17 01:25:45 +0100

    The icon of the game now shows up

--- lieb-revoice-festival/src/main/java/lieb/revoice/festival/Program.java
+++ lieb-revoice-festival/src/main/java/lieb/revoice/festival/Program.java
@@ -1,13 +1,35 @@
 package lieb.revoice.festival;
 
 import de.gurkenlabs.litiengine.Game;
+import de.gurkenlabs.litiengine.resources.Resources;
+import de.gurkenlabs.litiengine.environment.tilemap.IMap;
 
 public class Program {
 
 	public static void main(String[] args) {
-		Game.init(args);
-		Game.start();
-
+		
+		// Set information about the game
+	    Game.info().setName("Revoice Festival");
+	    Game.info().setSubTitle("");
+	    Game.info().setVersion("v0.1");
+	    Game.info().setWebsite("https://github.com/RicardoCQB/lieb-revoice-festival");
+	    Game.info().setDescription("2D Platformer made with LITengine,"
+	    		+ " also the jumps are controled with real life movements using an acelerometer.");
+	    
+	    // Initiates the game infrastructure.
+	    Game.init(args);	    
+	    
+	    // Load data from the utiLITI game file
+	    Resources.load("game.litidata");
+	    
+	    // Set the icon for the game
+	    Game.window().setIcon(Resources.images().get("icon.png"));
+	    Game.graphics().setBaseRenderScale(4.001f);
+	    
+	   
+	    
+	    Game.start();
+	    
 	}
 
 }
