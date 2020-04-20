c1b223ac567216e03b19b655c679ea57de315b54
RicardoCQB <ricardo_cqb@hotmail.com>
2020-04-20 23:53:45 +0100

    added accelerometer library

---
+++ lieb-revoice-festival/maps/forest/forest background 3000x3000.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="forest background 3000x3000" tilewidth="32" tileheight="32" tilecount="8649" columns="93">
+ <image source="forest background 3000x3000.png" width="3000" height="3000"/>
+</tileset>
